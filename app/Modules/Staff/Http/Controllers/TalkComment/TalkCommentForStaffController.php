3437adeb724e9028ff88d8894de78c3ef2e129ef
xowlsakffl <xowlsakffl@naver.com>
2026-04-22 12:03:58 +0900

    feat: 토크 메소드 정리

--- app/Modules/Staff/Http/Controllers/TalkComment/TalkCommentForStaffController.php
+++ app/Modules/Staff/Http/Controllers/TalkComment/TalkCommentForStaffController.php
@@ -4,16 +4,8 @@ namespace App\Modules\Staff\Http\Controllers\TalkComment;
 
 use App\Common\Http\Controllers\Controller;
 use App\Common\Http\Responses\ApiResponse;
-use App\Domains\Talk\Actions\Staff\TalkCommentCreateForStaffAction;
-use App\Domains\Talk\Actions\Staff\TalkCommentDeleteForStaffAction;
-use App\Domains\Talk\Actions\Staff\TalkCommentGetForStaffAction;
 use App\Domains\Talk\Actions\Staff\TalkCommentListForStaffAction;
-use App\Domains\Talk\Actions\Staff\TalkCommentUpdateForStaffAction;
-use App\Domains\Talk\Models\TalkComment;
-use App\Modules\Staff\Http\Requests\TalkComment\TalkCommentCreateForStaffRequest;
-use App\Modules\Staff\Http\Requests\TalkComment\TalkCommentGetForStaffRequest;
 use App\Modules\Staff\Http\Requests\TalkComment\TalkCommentListForStaffRequest;
-use App\Modules\Staff\Http\Requests\TalkComment\TalkCommentUpdateForStaffRequest;
 
 /**
  * TalkCommentForStaffController 역할 정의.
@@ -28,31 +20,4 @@ final class TalkCommentForStaffController extends Controller
         return ApiResponse::success($result['items'], $result['meta'] ?? null);
     }
 
-    public function getCommentForStaff(TalkComment $comment, TalkCommentGetForStaffRequest $request, TalkCommentGetForStaffAction $action)
-    {
-        $result = $action->execute($comment, $request->filters());
-
-        return ApiResponse::success($result['comment'] ?? $result);
-    }
-
-    public function storeCommentForStaff(TalkCommentCreateForStaffRequest $request, TalkCommentCreateForStaffAction $action)
-    {
-        $result = $action->execute($request->validated());
-
-        return ApiResponse::success($result['comment'] ?? $result);
-    }
-
-    public function updateCommentForStaff(TalkComment $comment, TalkCommentUpdateForStaffRequest $request, TalkCommentUpdateForStaffAction $action)
-    {
-        $result = $action->execute($comment, $request->validated());
-
-        return ApiResponse::success($result['comment'] ?? $result);
-    }
-
-    public function deleteCommentForStaff(TalkComment $comment, TalkCommentDeleteForStaffAction $action)
-    {
-        $result = $action->execute($comment);
-
-        return ApiResponse::success($result);
-    }
 }

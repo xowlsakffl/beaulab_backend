0b5de32df946331fff65b8fb821d428cf8e9569f
xowlsakffl <xowlsakffl@naver.com>
2026-09-03 14:50:47 +0900

    perf: API CORS 사전 요청 캐시 적용

---
+++ config/cors.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => ['*'],
+    'allowed_origins_patterns' => [],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => max(0, (int) env('CORS_MAX_AGE', 3600)),
+    'supports_credentials' => false,
+];
